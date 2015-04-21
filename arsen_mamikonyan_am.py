f33e1458e875a18c428a8903f7d1bb7ce85044ce
Arsen Mamikonyan <arsen@mamikonyan.am>
2015-04-21 11:03:25 +0100

    Port my incomplete website

--- arsen_mamikonyan_am.py
+++ arsen_mamikonyan_am.py
@@ -2,6 +2,38 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return render_template("index.html")
+@app.route('/')
+def index():
+    interests = [{
+        'name': 'Photography',
+        'url': 'http://ppd.arsen.mamikonyan.am/',
+      },
+      {
+        'name': 'Cooking',
+        'url': 'http://cook.arsen.mamikonyan.am/',
+      },
+      {
+        'name': 'Soccer',
+      },
+      {
+        'name': '3d printing',
+      },
+      {
+        'name': 'Scuba Diving',
+      }]
+    profiles = [
+      {
+        'service': 'facebook',
+        'url': 'https://www.facebook.com/arsen.mamikonyan',
+      },
+      {
+        'service': 'twitter',
+        'url': 'https://twitter.com/mamikonyana',
+      },
+      {
+        'service': 'github',
+        'url': 'https://github.com/mamikonyana'
+      }]
+    return render_template('index.html',
+                           interests=interests,
+                           profiles=profiles)
