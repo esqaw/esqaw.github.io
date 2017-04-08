1479871d4bb454755e74b2a07941c13af698659b
Arsen Mamikonyan <arsen@mamikonyan.am>
2017-04-09 00:19:07 +0400

    2_validation slides

--- fix_ml_html_statics.sh
+++ fix_ml_html_statics.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-find templates/ml_lectures -type f | xargs sed -i '' 's+src="presentation_data+src="/static/ml_lectures/presentation_data+'
+find templates/ml_lectures -type f | xargs gsed -i 's+src="presentation_data+src="/static/ml_lectures/presentation_data+'
+
+find templates/ml_lectures -type f | xargs gsed -i 's+"reveal.js/+"/static/reveal.js/+'
+find templates/ml_lectures -type f | xargs gsed -i "s+'reveal.js/+'/static/reveal.js/+"
