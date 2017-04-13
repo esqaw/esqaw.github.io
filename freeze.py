a0e5bea3f467632a96e68bc494523f07b64e7383
Arsen Mamikonyan <arsen@mamikonyan.am>
2017-04-13 20:49:28 +0400

    fix generators

--- freeze.py
+++ freeze.py
@@ -6,23 +6,23 @@ freezer = Freezer(app)
 
 
 @freezer.register_generator
-def ml_afternoon():
+def ml_afternoon_lectures():
     path = os.path.join(app.root_path, 'templates/ml_afternoon/')
     files = next(os.walk(path))[2]
     for _file in files:
         if _file.endswith('.html'):
-            print(_file)
             yield {'lecture_name': os.path.splitext(_file)[0]}
 
 
 @freezer.register_generator
-def ml_evening():
+def ml_evening_lectures():
     path = os.path.join(app.root_path, 'templates/ml_evening/')
     files = next(os.walk(path))[2]
     for _file in files:
         if _file.endswith('.html'):
             print(_file)
             yield {'lecture_name': os.path.splitext(_file)[0]}
+
 
 if __name__ == '__main__':
     freezer.freeze()
