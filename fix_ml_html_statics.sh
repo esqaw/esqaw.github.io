8a823345b0bdc1f92d340549e1e2fae86a6ceebf
Arsen Mamikonyan <arsen@mamikonyan.am>
2017-04-13 21:02:56 +0400

    more reorg

--- fix_ml_html_statics.sh
+++ fix_ml_html_statics.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-find templates/ml_lectures -type f | xargs gsed -i 's+src="presentation_data+src="/static/ml_lectures/presentation_data+'
+find templates/ml_afternoon -type f | xargs gsed -i 's+src="presentation_data+src="/static/ml_afternoon/presentation_data+'
+find templates/ml_afternoon -type f | xargs gsed -i 's+"reveal.js/+"/static/reveal.js/+'
+find templates/ml_afternoon -type f | xargs gsed -i "s+'reveal.js/+'/static/reveal.js/+"
 
-find templates/ml_lectures -type f | xargs gsed -i 's+"reveal.js/+"/static/reveal.js/+'
-find templates/ml_lectures -type f | xargs gsed -i "s+'reveal.js/+'/static/reveal.js/+"
+find templates/ml_evening -type f | xargs gsed -i 's+src="presentation_data+src="/static/ml_evening/presentation_data+'
+find templates/ml_evening -type f | xargs gsed -i 's+"reveal.js/+"/static/reveal.js/+'
+find templates/ml_evening -type f | xargs gsed -i "s+'reveal.js/+'/static/reveal.js/+"
